a2d55e8bf26b7753116de29b3b04af25fded63e9
Tim Burke <tim.burke@uselessorbit.org>
2025-11-30 19:27:29 +0000

    Adds check_tb_digest_to_hex and test cases

--- src/tb_crypt_lib_test.c
+++ src/tb_crypt_lib_test.c
@@ -11,9 +11,11 @@
 
 int check_tb_new_digest();
 
+int check_tb_digest_to_hex();
+int check_tb_digest_to_hex_single(int* tests, int* passes, int length);
+
 int check_tb_hex_to_digest();
 int check_tb_hex_to_digest_single(int* tests, int* passes, int length, int hex_only);
-
 
 int test_crypt_lib() {
   printf("Testing:  crypt_lib\n");
@@ -21,7 +23,7 @@ int test_crypt_lib() {
   int pass = 1;
 
   pass &= check_tb_new_digest() == SUCCESS;
-
+  pass &= check_tb_digest_to_hex() == SUCCESS;
   pass &= check_tb_hex_to_digest() == SUCCESS;
 
   printf("crypt_lib: %s\n\n", pass ? "PASS" : "FAIL");
@@ -61,6 +63,60 @@ int check_tb_new_digest() {
 
   TB_TEST_END("tb_new_digest:    ");
 }
+
+int check_tb_digest_to_hex() {
+  TB_TEST_START
+
+  padded_str padded;
+
+  int lengths[] = {DIGEST_SIZE - 2, DIGEST_SIZE - 1, DIGEST_SIZE, DIGEST_SIZE + 1, DIGEST_SIZE + 2, -1};
+
+  for (int i = 0; lengths[i] != -1; i++) {
+    check_tb_digest_to_hex_single(&tests, &passes, lengths[i]);
+  }
+
+  TB_TEST_END("tb_digest_to_hex: ");
+}
+
+int check_tb_digest_to_hex_single(int* tests, int* passes, int length) {
+  TB_SINGLE_TEST_START
+
+  padded_text _src;
+  digest* src = text_from_padded(&_src);
+  tb_new_text(src);
+
+  padded_text _dst;
+  text* dst = text_from_padded(&_dst);
+  tb_new_digest(dst);
+
+  padded_text _dst2;
+  text* dst2 = text_from_padded(&_dst2);
+  tb_new_text(dst2);
+
+  int exp = SUCCESS;
+
+  for (int i = 0; i < length; i++) {
+    src->text[i] = i;
+  }
+
+  int result = tb_digest_to_hex(dst, src);
+
+  if (result != exp) {
+    return !SUCCESS;
+  }
+
+  if (exp == SUCCESS) {
+    tb_text_to_hex(dst2, src, DIGEST_SIZE);
+    for (int i = 0; i < DIGEST_SIZE * 2; i++) {
+      if (dst->text[i] != dst2->text[i]) {
+        return !SUCCESS;
+      }
+    }
+  }
+
+  TB_SINGLE_TEST_END
+}
+
 
 int check_tb_hex_to_digest() {
   TB_TEST_START
